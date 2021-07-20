2bc5a00fe75bbe4076184f9e00592052be24500e
Suresh Appana <suresh.appana@alation.com>
2021-07-20 18:09:27 -0400

    Breaking installer to multiple files

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -7,7 +7,7 @@ mkdir -p $MODULES_DIR
 mkdir -p $KURL_PATCH_DIR
 
 echo "Copy res content from source code to archiving directory"
-cp -a ${{ github.workspace }}/installer/res/. $RESOURCE_DIR/
+cp -a $GITHUB_WORKSPACE/installer/res/. $RESOURCE_DIR/
 
 echo "Create versions and install config file"
 touch $VERSIONS_FILE $INSTALL_CONFIG_FILE
