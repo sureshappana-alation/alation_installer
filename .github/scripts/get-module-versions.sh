ef570303665b1fd29d6e8e222a7c38359d9c7591
Suresh Appana <suresh.appana@alation.com>
2021-07-20 19:37:09 -0400

    Breaking installer to multiple files

--- .github/scripts/get-module-versions.sh
+++ .github/scripts/get-module-versions.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-
 modulesList=()
 shopt -s nullglob
 
 # Read version files
 for file in ./versions/*.env; do
-  while IFS='=' read -r key val
+  while IFS='=' read -r module version
   do
-    # Ignore KURL from modules list as KURL has special treatment
-    if [[ $key != *"KURL"* ]]; then
-      modulesList+=($key)
-    fi
+    # Override application versions key name
+    input_module="INPUT_$module"
+    echo "$module=${!input_module:-$version}" >> $GITHUB_ENV
+    modulesList+=($module)
   done < "$file"
-  cat "$file"
-  echo
-done  >> $GITHUB_ENV
+done
+
+IFS=',' read -r -a excludeModulesList <<< "$EXCLUDE_MODULES_STRING"
+
+# Remove the modules in exclude list for processing
+for del in ${excludeModulesList[@]}
+do
+   modulesList=("${modulesList[@]/$del}")
+done
+
+# Remove KURL from final modules list as kurl has special processing
+modulesList=("${modulesList[@]/KURL}")
+echo ${modulesList[@]}
+
 echo ::set-output name=modulesList::${modulesList[@]}
