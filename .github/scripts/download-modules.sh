d357a69a63b82009c782e2618620e29f15cdb85c
Suresh Appana <suresh.appana@alation.com>
2021-07-20 18:11:34 -0400

    Breaking installer to multiple files

--- .github/scripts/download-modules.sh
+++ .github/scripts/download-modules.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-for i in ${{ env.modules }}; do
-module="${i}"
+for i in $modules; do
+  module="${i}"
 
-# Add entry to versions file
-echo "$module=${!module}" >> ${{ env.VERSIONS_FILE }}
+  # Add entry to versions file
+  echo "$module=${!module}" >> $VERSIONS_FILE
 
-# Download files from S3 only if version is not null and skip
-if [[ ${!module} = "skip" ]] || [[ -z ${!module} ]]; then
+  # Download files from S3 only if version is not null and skip
+  if [[ ${!module} = "skip" ]] || [[ -z ${!module} ]]; then
     echo "Skipping $module"
-else
+  else
     moduleFullName="${module}-${!i}.tar.gz"
     echo "Downloading $moduleFullName from S3"
-    aws s3 cp ${{ env.S3_DEV_BUCKET_URL }}/${moduleFullName,,} ${{env.MODULES_DIR}}/${module,,}/
-fi
+    aws s3 cp $S3_DEV_BUCKET_URL/${moduleFullName,,} $MODULES_DIR/${module,,}/
+  fi
 done
--- .github/scripts/generate-alation-install.sh
+++ .github/scripts/generate-alation-install.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 for dir in $(find $MODULES_DIR -mindepth 1 -maxdepth 1 -type d); do
-    directory=${dir%*/}
+  directory=${dir%*/}
 
-    echo "[$directory]: Started processing"
-    files=$(find $directory -mindepth 1 -maxdepth 1 -iname "*.tar.gz")
+  echo "[$directory]: Started processing"
+  files=$(find $directory -mindepth 1 -maxdepth 1 -iname "*.tar.gz")
 
-    if [ "${#files}" -gt 1 ]; then
-        moduleFile=${files[0]}
+  if [ "${#files}" -gt 1 ]; then
+    moduleFile=${files[0]}
 
-        echo "[$directory]: Found $moduleFile. Extracting..."
-        tar -xzf $moduleFile -C $directory  --strip-components=1
-        
-        echo "[$directory]: Deleting $moduleFile"
-        rm -f $moduleFile
-    else
-        echo "[$directory]: No module tar files found"
-    fi
+    echo "[$directory]: Found $moduleFile. Extracting..."
+    tar -xzf $moduleFile -C $directory  --strip-components=1
+    
+    echo "[$directory]: Deleting $moduleFile"
+    rm -f $moduleFile
+  else
+    echo "[$directory]: No module tar files found"
+  fi
 
-    if test -f "$directory/install.yaml"; then
-        echo "[$directory]: Found install.yaml"
-        echo "[$directory]: Appending install.yaml content to ${{ env.INSTALL_CONFIG_FILE }} file"
-        
-        # Creating any entry for module in alation_install.yaml
-        echo "${directory##*/}:" >> ${{ env.INSTALL_CONFIG_FILE }}
-        
-        # Adding contents of install.yaml to alation_install.yaml with indentation
-        sed -e 's/^/  /' $directory/install.yaml >> ${{ env.INSTALL_CONFIG_FILE }}
-    else
-        echo "[$directory]: $directory/install.yaml" doesn\'t exists. Ignoring...
-    fi
+  if test -f "$directory/install.yaml"; then
+    echo "[$directory]: Found install.yaml"
+    echo "[$directory]: Appending install.yaml content to ${{ env.INSTALL_CONFIG_FILE }} file"
+    
+    # Creating any entry for module in alation_install.yaml
+    echo "${directory##*/}:" >> ${{ env.INSTALL_CONFIG_FILE }}
+    
+    # Adding contents of install.yaml to alation_install.yaml with indentation
+    sed -e 's/^/  /' $directory/install.yaml >> ${{ env.INSTALL_CONFIG_FILE }}
+  else
+    echo "[$directory]: $directory/install.yaml" doesn\'t exists. Ignoring...
+  fi
 done
