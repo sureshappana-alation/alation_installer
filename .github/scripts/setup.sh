f761a04e9d79f35013085691f97c054a7964ebe4
Suresh Appana <suresh.appana@alation.com>
2021-07-22 17:51:03 -0400

    Set environment variablesg

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# set env variables
+echo ALATION_OUTPUT_DIR=alation-$VERSION >> $GITHUB_ENV
+echo ALATION_OUTPUT=$ALATION_OUTPUT_DIR.tar.gz  >> $GITHUB_ENV
+echo S3_DEV_BUCKET_URL: "s3://${{ secrets.S3_DEV_BUCKET_NAME }}" >> $GITHUB_ENV
+echo S3_RELEASE_BUCKET_URL: "s3://${{ secrets.S3_RELEASE_BUCKET_NAME }}" >> $GITHUB_ENV
+echo BASE_DIR: ./alation >> $GITHUB_ENV
+echo RESOURCE_DIR: ./alation/res >> $GITHUB_ENV
+echo MODULES_DIR: ./alation/res/modules >> $GITHUB_ENV
+echo KURL_PATCH_DIR: ./alation/res/kurl_patch >> $GITHUB_ENV
+echo VERSIONS_FILE: ./alation/versions.txt >> $GITHUB_ENV
+echo INSTALL_CONFIG_FILE: ./alation/alation_install.yaml >> $GITHUB_ENV
+
 echo "Create required directories"
 
 mkdir -p $BASE_DIR
@@ -20,15 +32,3 @@ NOW=$(date +'%Y%m%d')
 BRANCH=${GITHUB_REF##*/}
 
 VERSION=alation-k8s-$branch-$NOW.$GITHUB_RUN_NUMBER
-
-# set env variables
-echo ALATION_OUTPUT_DIR=alation-$VERSION >> $GITHUB_ENV
-echo ALATION_OUTPUT=$ALATION_OUTPUT_DIR.tar.gz  >> $GITHUB_ENV
-echo S3_DEV_BUCKET_URL: "s3://${{ secrets.S3_DEV_BUCKET_NAME }}" >> $GITHUB_ENV
-echo S3_RELEASE_BUCKET_URL: "s3://${{ secrets.S3_RELEASE_BUCKET_NAME }}" >> $GITHUB_ENV
-echo BASE_DIR: ./alation >> $GITHUB_ENV
-echo RESOURCE_DIR: ./alation/res >> $GITHUB_ENV
-echo MODULES_DIR: ./alation/res/modules >> $GITHUB_ENV
-echo KURL_PATCH_DIR: ./alation/res/kurl_patch >> $GITHUB_ENV
-echo VERSIONS_FILE: ./alation/versions.txt >> $GITHUB_ENV
-echo INSTALL_CONFIG_FILE: ./alation/alation_install.yaml >> $GITHUB_ENV
