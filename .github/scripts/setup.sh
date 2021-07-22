c3c2faa04fd181194c01082437869a11ee40a9d8
Suresh Appana <suresh.appana@alation.com>
2021-07-22 18:05:10 -0400

    Updated variables

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+BASE_DIR=./alation
+RESOURCE_DIR=$BASE_DIR/res
+
 # set env variables
 echo ALATION_OUTPUT_DIR=alation-$VERSION >> $GITHUB_ENV
 echo ALATION_OUTPUT=$ALATION_OUTPUT_DIR.tar.gz  >> $GITHUB_ENV
-echo S3_DEV_BUCKET_URL: "s3://${{ secrets.S3_DEV_BUCKET_NAME }}" >> $GITHUB_ENV
-echo S3_RELEASE_BUCKET_URL: "s3://${{ secrets.S3_RELEASE_BUCKET_NAME }}" >> $GITHUB_ENV
-echo BASE_DIR: ./alation >> $GITHUB_ENV
-echo RESOURCE_DIR: ./alation/res >> $GITHUB_ENV
-echo MODULES_DIR: ./alation/res/modules >> $GITHUB_ENV
-echo KURL_PATCH_DIR: ./alation/res/kurl_patch >> $GITHUB_ENV
-echo VERSIONS_FILE: ./alation/versions.txt >> $GITHUB_ENV
-echo INSTALL_CONFIG_FILE: ./alation/alation_install.yaml >> $GITHUB_ENV
+echo BASE_DIR=$BASE_DIR >> $GITHUB_ENV
+echo RESOURCE_DIR=$RESOURCE_DIR >> $GITHUB_ENV
+echo MODULES_DIR=$RESOURCE_DIR/modules >> $GITHUB_ENV
+echo KURL_PATCH_DIR=$RESOURCE_DIR/kurl_patch >> $GITHUB_ENV
+echo VERSIONS_FILE=$BASE_DIR/versions.txt >> $GITHUB_ENV
+echo INSTALL_CONFIG_FILE=$BASE_DIR/alation_install.yaml >> $GITHUB_ENV
 
 echo "Create required directories"
 
