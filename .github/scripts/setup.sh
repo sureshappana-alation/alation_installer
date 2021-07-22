f56093b3c1675dee00d5202726b3acf417ab3797
Suresh Appana <suresh.appana@alation.com>
2021-07-22 17:39:34 -0400

    Removed AA and added build output version

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -11,3 +11,17 @@ cp -a $GITHUB_WORKSPACE/installer/res/. $RESOURCE_DIR/
 
 echo "Create versions and install config file"
 touch $VERSIONS_FILE $INSTALL_CONFIG_FILE
+
+
+# Get current date
+NOW=$(date +'%Y%m%d')
+
+# Get branch name
+BRANCH=${GITHUB_REF##*/}
+
+VERSION=alation-k8s-$branch-$NOW.$GITHUB_RUN_NUMBER
+
+# set build version as env variables
+
+echo ALATION_OUTPUT_DIR=alation-$VERSION >> $GITHUB_ENV
+echo ALATION_OUTPUT=$ALATION_OUTPUT_DIR.tar.gz  >> $GITHUB_ENV
