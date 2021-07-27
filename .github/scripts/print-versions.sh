043c267c48ea96cbce7462795219b8f8debb00b0
Suresh Appana <suresh.appana@alation.com>
2021-07-27 19:17:50 -0400

    Alation fat container information added

--- .github/scripts/print-versions.sh
+++ .github/scripts/print-versions.sh
@@ -4,3 +4,13 @@ echo $INPUT_CONTEXT
 
 echo "Printing2"
 echo $INPUT_CONTEXT1
+
+echo "Printing2"
+echo ${INPUT_CONTEXT1[ALATIONANALYTICS]}
+
+export x=ALATIONANALYTICS
+
+echo "Printing2"
+echo ${INPUT_CONTEXT1.ALATIONANALYTICS} | jq ."$x"
+
+
