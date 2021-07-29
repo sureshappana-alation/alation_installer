7b1e4ab49357a0c8523751b5df6ca1bf26ab9671
Suresh Appana <suresh.appana@alation.com>
2021-07-29 10:51:43 -0400

    updated alation install version file

--- .github/scripts/get-module-versions-new.sh
+++ .github/scripts/get-module-versions-new.sh
@@ -19,7 +19,8 @@ IFS=',' read -r -a excludeModulesList <<< "$EXCLUDE_MODULES_STRING"
 # Remove the modules in exclude list for processing
 for del in ${excludeModulesList[@]}
 do
-   modulesList=("${modulesList[@]/$del}")
+  echo "working on delete module: $del"
+  modulesList=("${modulesList[@]/$del}")
 done
 
 # Remove KURL from final modules list as kurl has special processing
