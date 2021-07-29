153e59548f41d0bd86a3df3de99e5326f7ac57ef
Suresh Appana <suresh.appana@alation.com>
2021-07-29 10:28:01 -0400

    updated alation install version file

---
+++ .github/scripts/get-module-versions-new.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+modulesList=()
+shopt -s nullglob
+
+# Read version files
+for file in ./versions/*.env; do
+  while IFS='=' read -r module version
+  do
+    echo "$module=$version" >> $GITHUB_ENV
+    modulesList+=($module)
+  done < "$file"
+done
+
+IFS=',' read -r -a excludeModulesList <<< "$EXCLUDE_MODULES_STRING"
+
+# Remove the modules in exclude list for processing
+for del in ${excludeModulesList[@]}
+do
+   modulesList=("${modulesList[@]/$del}")
+done
+
+# Remove KURL from final modules list as kurl has special processing
+modulesList=("${modulesList[@]/KURL}")
+echo ${modulesList[@]}
+
+echo ::set-output name=modulesList::${modulesList[@]}
