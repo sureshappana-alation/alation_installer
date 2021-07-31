d6aacf3fb9ca113b3400f37e8e58581d5e0612f1
Suresh Appana <suresh.appana@alation.com>
2021-07-31 15:23:37 -0400

    Terraform workflow added

--- .github/terraform/output.tf
+++ .github/terraform/output.tf
@@ -3,17 +3,17 @@ output "vpc_id" {
   description = "The vpc id created"
 }
 output "instance_private_ip_addr" {
-  value = aws_instance.server.private_ip
+  value = aws_instance.alation_aws_instance.private_ip
   description = "The private IP address of the EC2 server instance."
 }
 
 output "instance_public_ip_addr" {
-  value = aws_instance.server.public_ip
+  value = aws_instance.alation_aws_instance.public_ip
   description = "The public IP address of the EC2 server instance."
 }
 
 output "instance_name" {
-  value = aws_instance.server.name
+  value = aws_instance.alation_aws_instance.name
   description = "The Name of the EC2 server instance."
 }
 
