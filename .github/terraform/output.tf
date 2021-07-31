80ba9b38ae005bd611f6f16f685f1d49b0dbe7c5
Suresh Appana <suresh.appana@alation.com>
2021-07-31 15:29:10 -0400

    Terraform workflow added

--- .github/terraform/output.tf
+++ .github/terraform/output.tf
@@ -11,13 +11,3 @@ output "instance_public_ip_addr" {
   value = aws_instance.alation_aws_instance.public_ip
   description = "The public IP address of the EC2 server instance."
 }
-
-output "instance_name" {
-  value = aws_instance.alation_aws_instance.name
-  description = "The Name of the EC2 server instance."
-}
-
-output "complete_details" {
-  value = aws_instance
-  description = "AWS Instance complete details"
-}
