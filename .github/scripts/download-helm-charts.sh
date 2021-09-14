0cfff68b7a85ed491ef64e3faa8714786ad22cd8
Suresh Appana <suresh.appana@alation.com>
2021-09-13 20:27:19 -0400

    Build pipeline updated as per new approach

--- .github/scripts/download-helm-charts.sh
+++ .github/scripts/download-helm-charts.sh
@@ -30,3 +30,15 @@ bash --version
 #     helm chart export 248135293344.dkr.ecr.us-east-2.amazonaws.com/$module:${!module}
 #   fi
 # done
+
+
+declare -A acronyms
+acronyms[ACK]=Acknowledgement
+acronyms[BGP]="Border Gateway Protocol"
+acronyms[CIDR]="Classless Inter-Domain Routing"
+acronyms[DHCP]="Dynamic Host Configuration Protocol"
+acronyms[EOF]="End of Frame"
+echo ${acronyms[ACK]}
+echo ${acronyms[DHCP]}
+
+echo ::set-output name=acronyms::${acronyms[@]}
