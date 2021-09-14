6b25314eb81321f004de24fe931915a200e245d4
Suresh Appana <suresh.appana@alation.com>
2021-09-14 11:25:59 -0400

    Build pipeline updated as per new approach

--- .github/scripts/download-helm-charts.sh
+++ .github/scripts/download-helm-charts.sh
@@ -1,44 +1,26 @@
 #!/bin/bash
 
-echo "Printing env"
-# printenv
-bash --version
+aws ecr get-login-password \
+     --region us-east-2 | helm registry login \
+     --username AWS \
+     --password-stdin $ECR_URL
 
-# export HELM_EXPERIMENTAL_OCI=1
+for i in $modules; do
+  module="${i}"
 
-# aws ecr get-login-password \
-#      --region us-east-2 | helm registry login \
-#      --username AWS \
-#      --password-stdin $ECR_URL
+  echo "aa version is: " ${alation-analytics}
+  # Add entry to versions file
+  echo "$module=${!module}" >> $VERSIONS_FILE
 
-# for i in $modules; do
-#   module="${i}"
-
-#   echo "aa version is: " ${alation-analytics}
-#   # Add entry to versions file
-#   echo "$module=${!module}" >> $VERSIONS_FILE
-
-#   # Download files from S3 only if version is not null
-#   if [[ -z ${!module} ]]; then
-#     echo "Skipping $module"
-#   else
-#     moduleFullName="${module}-${!i}.tar.gz"
-#     echo "Pulling helm chart $module"
-#     # aws s3 cp $S3_DEV_BUCKET_URL/${moduleFullName,,} $MODULES_DIR/${module,,}/
-#     helm chart pull 248135293344.dkr.ecr.us-east-2.amazonaws.com/$module:${!module}
-#     echo "Exporting helm chart $module"
-#     helm chart export 248135293344.dkr.ecr.us-east-2.amazonaws.com/$module:${!module}
-#   fi
-# done
-
-
-declare -A acronyms
-acronyms[ACK]=Acknowledgement
-acronyms[BGP]="Border Gateway Protocol"
-acronyms[CIDR]="Classless Inter-Domain Routing"
-acronyms[DHCP]="Dynamic Host Configuration Protocol"
-acronyms[EOF]="End of Frame"
-echo ${acronyms[ACK]}
-echo ${acronyms[DHCP]}
-
-echo ::set-output name=acronyms::${acronyms[@]}
+  # Download files from S3 only if version is not null
+  if [[ -z ${!module} ]]; then
+    echo "Skipping $module"
+  else
+    moduleFullName="${module}-${!i}.tar.gz"
+    echo "Pulling helm chart $module"
+    # aws s3 cp $S3_DEV_BUCKET_URL/${moduleFullName,,} $MODULES_DIR/${module,,}/
+    helm chart pull 248135293344.dkr.ecr.us-east-2.amazonaws.com/$module:${!module}
+    echo "Exporting helm chart $module"
+    helm chart export 248135293344.dkr.ecr.us-east-2.amazonaws.com/$module:${!module}
+  fi
+done
--- .github/scripts/get-module-versions.sh
+++ .github/scripts/get-module-versions.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
-modulesList=()
-shopt -s nullglob
 
-# Read version files
-for file in ./versions/*.env; do
-  while IFS='=' read -r module version
-  do
-    echo "$module=$version" >> $GITHUB_ENV
-    modulesList+=($module)
-  done < "$file"
-done
+# modulesList=()
+# shopt -s nullglob
 
-# Override versions information
-echo ${INPUT_CONTEXT} | jq -r 'to_entries[] | "\(.key)=\(.value)"' >> $GITHUB_ENV
+# # Read version files
+# for file in ./versions/*.env; do
+#   while IFS='=' read -r module version
+#   do
+#     echo "$module=$version" >> $GITHUB_ENV
+#     modulesList+=($module)
+#   done < "$file"
+# done
 
-IFS=',' read -r -a excludeModulesList <<< "$EXCLUDE_MODULES_STRING"
+# # Override versions information
+# echo ${INPUT_CONTEXT} | jq -r 'to_entries[] | "\(.key)=\(.value)"' >> $GITHUB_ENV
 
-# Remove the modules in exclude list for processing
-for del in ${excludeModulesList[@]}
-do
-  echo "working on delete module: $del"
-  modulesList=("${modulesList[@]/$del}")
-done
+# IFS=',' read -r -a excludeModulesList <<< "$EXCLUDE_MODULES_STRING"
 
-# Remove KURL from final modules list as kurl has special processing
-modulesList=("${modulesList[@]/KURL}")
-echo ${modulesList[@]}
+# # Remove the modules in exclude list for processing
+# for del in ${excludeModulesList[@]}
+# do
+#   echo "working on delete module: $del"
+#   modulesList=("${modulesList[@]/$del}")
+# done
 
-echo ::set-output name=modulesList::${modulesList[@]}
+# # Remove KURL from final modules list as kurl has special processing
+# modulesList=("${modulesList[@]/KURL}")
+# echo ${modulesList[@]}
+
+# echo ::set-output name=modulesList::${modulesList[@]}
+
+export versions=$(jq -s add versions-json/*.json | jq @json)
+
+echo ::set-output name=versions::${versions[@]}
