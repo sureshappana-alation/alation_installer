3e1c87bccce63a0b2185039895de08eb02ad5d7d
Suresh Appana <suresh.appana@alation.com>
2021-09-14 20:15:14 -0400

    Build pipeline updated as per new approach

--- .github/scripts/get-module-versions.sh
+++ .github/scripts/get-module-versions.sh
@@ -15,21 +15,23 @@
 # # Override versions information
 # echo ${INPUT_CONTEXT} | jq -r 'to_entries[] | "\(.key)=\(.value)"' >> $GITHUB_ENV
 
-# IFS=',' read -r -a excludeModulesList <<< "$EXCLUDE_MODULES_STRING"
+IFS=',' read -r -a excludeModulesList <<< "$EXCLUDE_MODULES_STRING"
 
-# # Remove the modules in exclude list for processing
-# for del in ${excludeModulesList[@]}
-# do
-#   echo "working on delete module: $del"
-#   modulesList=("${modulesList[@]/$del}")
-# done
-
+FORMATTED_EXCLUDE_STRING=""
+# Remove the modules in exclude list for processing
+for module in ${excludeModulesList[@]}
+do
+  module=`echo $module | xargs`
+  FORMATTED_EXCLUDE_STRING+=".\""$module"\","
+done
+FORMATTED_EXCLUDE_STRING="${FORMATTED_EXCLUDE_STRING::-1}"
 # echo ${modulesList[@]}
 
 # echo ::set-output name=modulesList::${modulesList[@]}
 
 
-FORMATTED_EXCLUDE_MODULES_STRING=".\"${EXCLUDE_MODULES_STRING//,/\",.\"}\""
+# FORMATTED_EXCLUDE_MODULES_STRING=".\"${EXCLUDE_MODULES_STRING//,/\",.\"}\""
+echo $FORMATTED_EXCLUDE_STRING
 versions=$(jq -s add versions-json/*.json | jq 'del('$FORMATTED_EXCLUDE_STRING')' | jq @json)
 echo $versions
 
