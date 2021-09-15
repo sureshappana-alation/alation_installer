669860db37a0ead44edcc9c4b512d0904eaeb836
Suresh Appana <suresh.appana@alation.com>
2021-09-15 19:39:51 -0400

    Build pipeline updated as per new approach

--- .github/scripts/download-helm-charts.sh
+++ .github/scripts/download-helm-charts.sh
@@ -8,6 +8,7 @@ aws ecr get-login-password \
      --password-stdin $ECR_URL
 echo $modules | jq -r 'fromjson | to_entries[] | .key +" " + .value' | while IFS=' ' read -r key value; do 
   module="${key}"
+  moduleVersion="${value}"
 
   # Add entry to versions file
   echo "$key=$value" >> $VERSIONS_FILE
@@ -18,8 +19,10 @@ echo $modules | jq -r 'fromjson | to_entries[] | .key +" " + .value' | while IFS
   else
     echo "Pulling helm chart $module"
     # aws s3 cp $S3_DEV_BUCKET_URL/${moduleFullName,,} $MODULES_DIR/${module,,}/
-    helm chart pull $HELM_REGISTRY_URL/$module:${value}
+    helm chart pull $HELM_REGISTRY_URL/$module:$moduleVersion
     echo "Exporting helm chart $module"
-    helm chart export $HELM_REGISTRY_URL/$module:${value} -d $MODULES_DIR
+    helm chart export $HELM_REGISTRY_URL/$module:$moduleVersion -d $MODULES_DIR/$module/charts
+    # move helm chart files to parent and remove the module directory
+    mv $MODULES_DIR/$module/charts/$module/*  $MODULES_DIR/$module/charts && rm -rf $MODULES_DIR/$module/charts/$module
   fi
 done
--- .github/scripts/generate-alation-install-yaml.sh
+++ .github/scripts/generate-alation-install-yaml.sh
@@ -3,22 +3,22 @@ for dir in $(find $MODULES_DIR -mindepth 1 -maxdepth 1 -type d); do
   directory=${dir%*/}
 
   echo "[$directory]: Started processing"
-  files=$(find $directory -mindepth 1 -maxdepth 1 -iname "*.tar.gz")
+  # files=$(find $directory -mindepth 1 -maxdepth 1 -iname "*.tar.gz")
 
-  if [ "${#files}" -gt 1 ]; then
-    moduleFile=${files[0]}
+  # if [ "${#files}" -gt 1 ]; then
+  #   moduleFile=${files[0]}
 
-    echo "[$directory]: Found $moduleFile. Extracting..."
-    tar -xzf $moduleFile -C $directory  --strip-components=1
+  #   echo "[$directory]: Found $moduleFile. Extracting..."
+  #   tar -xzf $moduleFile -C $directory  --strip-components=1
     
-    echo "[$directory]: Deleting $moduleFile"
-    rm -f $moduleFile
-  else
-    echo "[$directory]: No module tar files found"
-  fi
+  #   echo "[$directory]: Deleting $moduleFile"
+  #   rm -f $moduleFile
+  # else
+  #   echo "[$directory]: No module tar files found"
+  # fi
 
   # Processing install.yaml file
-  if test -f "$directory/install.yaml"; then
+  if test -f "$directory/charts/install.yaml"; then
     echo "[$directory]: Found install.yaml"
     echo "[$directory]: Appending install.yaml content to $INSTALL_CONFIG_FILE file"
     
@@ -26,8 +26,8 @@ for dir in $(find $MODULES_DIR -mindepth 1 -maxdepth 1 -type d); do
     echo "${directory##*/}:" >> $INSTALL_CONFIG_FILE
     
     # Adding contents of install.yaml to alation_install.yaml with indentation
-    sed -e 's/^/  /' $directory/install.yaml >> $INSTALL_CONFIG_FILE
+    sed -e 's/^/  /' $directory/charts/install.yaml >> $INSTALL_CONFIG_FILE
   else
-    echo "[$directory]: $directory/install.yaml" doesn\'t exists. Ignoring...
+    echo "[$directory]: $directory/charts/install.yaml" doesn\'t exists. Ignoring...
   fi
 done
