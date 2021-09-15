91f85cd650437ea54783b931ac0086704b005b73
Suresh Appana <suresh.appana@alation.com>
2021-09-14 22:06:45 -0400

    Build pipeline updated as per new approach

--- .github/scripts/download-helm-charts.sh
+++ .github/scripts/download-helm-charts.sh
@@ -4,23 +4,20 @@ aws ecr get-login-password \
      --region us-east-2 | helm registry login \
      --username AWS \
      --password-stdin $ECR_URL
+echo $modules | jq -r 'to_entries[] | .key +" " + .value' | while IFS=' ' read -r key value; do 
+  module="${key}"
 
-for i in $modules; do
-  module="${i}"
-
-  echo "aa version is: " ${alation-analytics}
   # Add entry to versions file
-  echo "$module=${!module}" >> $VERSIONS_FILE
+  echo "$key=$version" >> $VERSIONS_FILE
 
   # Download files from S3 only if version is not null
-  if [[ -z ${!module} ]]; then
+  if [[ -z $version ]]; then
     echo "Skipping $module"
   else
-    moduleFullName="${module}-${!i}.tar.gz"
     echo "Pulling helm chart $module"
     # aws s3 cp $S3_DEV_BUCKET_URL/${moduleFullName,,} $MODULES_DIR/${module,,}/
-    helm chart pull 248135293344.dkr.ecr.us-east-2.amazonaws.com/$module:${!module}
+    helm chart pull 248135293344.dkr.ecr.us-east-2.amazonaws.com/$module:${version}
     echo "Exporting helm chart $module"
-    helm chart export 248135293344.dkr.ecr.us-east-2.amazonaws.com/$module:${!module}
+    helm chart export 248135293344.dkr.ecr.us-east-2.amazonaws.com/$module:${version}
   fi
 done
