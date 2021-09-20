3c0fa75226a2c31418cea4d9ebae27d488db9410
Suresh Appana <suresh.appana@alation.com>
2021-09-20 08:08:12 -0400

    Build pipeline updated as per new approach

--- .github/scripts/get-module-versions.sh
+++ .github/scripts/get-module-versions.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 
-# modulesList=()
-# shopt -s nullglob
+EXCLUDE_MODULES=$(echo ${INPUT_CONTEXT} | jq '.EXCLUDE_MODULES')
 
-# # Read version files
-# for file in ./versions/*.env; do
-#   while IFS='=' read -r module version
-#   do
-#     echo "$module=$version" >> $GITHUB_ENV
-#     modulesList+=($module)
-#   done < "$file"
-# done
+IFS=',' read -r -a excludeModulesList <<< "$EXCLUDE_MODULES"
 
-# # Override versions information
-# echo ${INPUT_CONTEXT} | jq -r 'to_entries[] | "\(.key)=\(.value)"' >> $GITHUB_ENV
+FORMATTED_EXCLUDE_MODULES=".EXCLUDE_MODULES,"
 
-
-
-IFS=',' read -r -a excludeModulesList <<< "$EXCLUDE_MODULES_STRING"
-
-FORMATTED_EXCLUDE_STRING=".EXCLUDE_MODULES_STRING,"
-# Remove the modules in exclude list for processing
+# Remove the modules in exclude list from modules list
 for module in ${excludeModulesList[@]}
 do
   module=`echo $module | xargs`
-  FORMATTED_EXCLUDE_STRING+=".\""$module"\","
+  FORMATTED_EXCLUDE_MODULES+=".\""$module"\","
 done
-FORMATTED_EXCLUDE_STRING="${FORMATTED_EXCLUDE_STRING%,}"
-echo $FORMATTED_EXCLUDE_STRING
-# versions=$(jq -s add versions-json/*.json | jq 'del('$FORMATTED_EXCLUDE_STRING')' | jq @json)
+
+FORMATTED_EXCLUDE_MODULES="${FORMATTED_EXCLUDE_MODULES%,}"
+
+echo $FORMATTED_EXCLUDE_MODULES
+
+# Merge version files
 versions=$(jq -s add versions-json/*.json)
 
 echo "base versions from versions/*.json: $versions"
@@ -38,9 +27,9 @@ versions=$(echo $versions $INPUT_CONTEXT | jq -s add)
 
 echo "versions after overriding: $versions"
 
-if [ ! -z "${FORMATTED_EXCLUDE_STRING}" ]
+if [ ! -z "${FORMATTED_EXCLUDE_MODULES}" ]
 then
-  versions=$(echo $versions | jq 'del('$FORMATTED_EXCLUDE_STRING')')
+  versions=$(echo $versions | jq 'del('$FORMATTED_EXCLUDE_MODULES')')
 fi
 
 # Final versions
