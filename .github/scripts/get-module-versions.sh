a732b45764a62be67ff1e1b98a67f877a9ecf0c9
Suresh Appana <suresh.appana@alation.com>
2021-09-23 18:29:18 -0400

    test

--- .github/scripts/get-module-versions.sh
+++ .github/scripts/get-module-versions.sh
@@ -3,27 +3,38 @@ set -e
 
 EXCLUDE_MODULES=$(echo $INPUT_CONTEXT | jq '.EXCLUDE_MODULES')
 
+# Remove quotes from the beginning and ending
+EXCLUDE_MODULES="${EXCLUDE_MODULES%\"}"
+EXCLUDE_MODULES="${EXCLUDE_MODULES#\"}"
+
 IFS=',' read -r -a excludeModulesList <<< "$EXCLUDE_MODULES"
 
-FORMATTED_EXCLUDE_MODULES=".EXCLUDE_MODULES,"
+FORMATTED_EXCLUDE_MODULES=".\"EXCLUDE_MODULES\","
 
+echo "Formatted modules list: $FORMATTED_EXCLUDE_MODULES"
 # Generate the module list to be excluded
 for module in ${excludeModulesList[@]}
 do
+  echo "[$module]: started processing"
   module=`echo $module | xargs`
   FORMATTED_EXCLUDE_MODULES+=".\""$module"\","
+  echo "Formatted modules list: $FORMATTED_EXCLUDE_MODULES"
+  echo "[$module]: Added to excluded list"
 done
 
 FORMATTED_EXCLUDE_MODULES="${FORMATTED_EXCLUDE_MODULES%,}"
 
+echo "Reading version json files"
 # Merge version files and override versions
-MODULE_VERSIONS=$(jq -s add versions-json/*.json)
+MODULE_VERSIONS=$(jq -s add versions/*.json)
 echo "Merged version json files: $MODULE_VERSIONS"
 
+echo "Applying override version on base versions"
 # Override versions
 MODULE_VERSIONS=$(echo $MODULE_VERSIONS $INPUT_CONTEXT | jq -s add)
 echo "Override versions applied: $MODULE_VERSIONS"
 
+echo "Removing excluded modules list from modules list"
 # Remove the excluded modules from final version list
 MODULE_VERSIONS=$(echo $MODULE_VERSIONS | jq 'del('$FORMATTED_EXCLUDE_MODULES')')
 echo "Excluded modules from EXCLUDE_MODULES: $MODULE_VERSIONS"
--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -17,14 +17,24 @@ touch $VERSIONS_FILE $INSTALL_CONFIG_FILE
 # Creating modules entry in alation-install.yaml
 echo "modules:" >> $INSTALL_CONFIG_FILE
 
-# Get current date
-NOW=$(date +'%Y%m%d')
+VERSION_FILE=$GITHUB_WORKSPACE/version.env
 
-# Get branch name
-BRANCH=${GITHUB_REF##*/}
+# check if file exist in repo, exit 1 if file does not exist
+if [[ ! -f "$VERSION_FILE" ]]
+then
+    echo "ERROR: $VERSION_FILE does not exist."
+    exit 1
+fi
 
-ALATION_OUTPUT=alation-k8s-$BRANCH-$NOW.$GITHUB_RUN_NUMBER
+# Load version file to environment
+. $VERSION_FILE
+
+echo MAJOR=$MAJOR >> $GITHUB_ENV
+echo MINOR=$MINOR >> $GITHUB_ENV
+echo PATCH=$PATCH >> $GITHUB_ENV
+
+BUILD_VERSION=$MAJOR.$MINOR.$PATCH.$GITHUB_RUN_NUMBER
 
 # set env variables
-echo ALATION_OUTPUT_DIR=$ALATION_OUTPUT >> $GITHUB_ENV
-echo ALATION_OUTPUT=$ALATION_OUTPUT.tar.gz  >> $GITHUB_ENV
+echo BUILD_VERSION=$BUILD_VERSION >> $GITHUB_ENV
+echo BUILD_OUTPUT=$BUILD_VERSION.tar.gz  >> $GITHUB_ENV
