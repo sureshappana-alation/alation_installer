4e0c19b6ca1bc61d322408f12a83478cbced1fef
Suresh Appana <suresh.appana@alation.com>
2021-09-29 15:28:55 -0400

    new workflow added

--- .github/scripts/configure.sh
+++ .github/scripts/configure.sh
@@ -9,4 +9,10 @@ pod=kubectl get pod -l app.kubernetes.io/name=alationfc
 
 kubectl wait --for=condition=ready pod $pod
 
-kubectl exec $pod -- bash -c ""
+kubectl exec $pod -- bash -c "service alation shell && \
+alation_conf -s True alation.ocf.acm.k8s && \
+alation_conf alation.feature_flags.enable_alation_analytics_v2 -s True && \
+alation_conf alation_analytics-v2.pgsql.config.host -s postgres && \
+alation_conf alation_analytics-v2.rmq.config.host -s rabbitmq && \
+alation_conf alation_analytics-v2.pgsql.config.port -s 5432 && \
+"
