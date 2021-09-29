c6222ca70111cc7f6dfd6e2af8d875428a151760
Suresh Appana <suresh.appana@alation.com>
2021-09-29 15:35:42 -0400

    new workflow added

--- .github/scripts/configure.sh
+++ .github/scripts/configure.sh
@@ -9,6 +9,8 @@ pod=kubectl get pod -l app.kubernetes.io/name=alationfc
 
 kubectl wait --for=condition=ready pod $pod
 
+echo "Pod name is: "$pod
+
 kubectl exec $pod -- bash -c "service alation shell && \
 alation_conf -s True alation.ocf.acm.k8s && \
 alation_conf alation.feature_flags.enable_alation_analytics_v2 -s True && \
