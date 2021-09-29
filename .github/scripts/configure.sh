babb8861c5b92f6168d94d9a19bb3fc7d378ab4b
Suresh Appana <suresh.appana@alation.com>
2021-09-29 15:36:38 -0400

    new workflow added

--- .github/scripts/configure.sh
+++ .github/scripts/configure.sh
@@ -5,7 +5,7 @@ set -e
 
 aws eks --region us-east-2 update-kubeconfig --name CloudInfraQAEKS
 
-pod=kubectl get pod -l app.kubernetes.io/name=alationfc
+pod=$(kubectl get pod -l app.kubernetes.io/name=alationfc)
 
 kubectl wait --for=condition=ready pod $pod
 
