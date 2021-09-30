fa6b3e3f04d2527993874fb15559799de2de9ab2
Suresh Appana <suresh.appana@alation.com>
2021-09-30 13:03:19 -0400

    new workflow added

--- .github/scripts/configure.sh
+++ .github/scripts/configure.sh
@@ -7,23 +7,22 @@ aws eks --region us-east-2 update-kubeconfig --name CloudInfraQAEKS
 
 pod=$(kubectl get pod -l app.kubernetes.io/name=alationfc -o jsonpath="{.items[0].metadata.name}")
 
-echo "Pod name is: "$pod
+echo "Waiting for Alation fat container pod "$pod " to become ready"
 
 kubectl wait --for=condition=ready pod $pod
 
-alation_config_commands1="alation_conf -s True alation.ocf.acm.k8s && \
+
+aa_postgres_pwd_alation_conf_cmd="alation_conf alation_analytics-v2.pgsql.password -s 'password@123'"
+
+aa_ocf_alation_conf_cmd="alation_conf -s True alation.ocf.acm.k8s && \
 alation_conf alation.feature_flags.enable_alation_analytics_v2 -s True && \
 alation_conf alation_analytics-v2.pgsql.config.host -s postgres && \
 alation_conf alation_analytics-v2.rmq.config.host -s rabbitmq && \
 alation_conf alation_analytics-v2.pgsql.config.port -s 5432 && \
 alation_supervisor restart web:* celery:*"
 
-alation_config_commands2="alation_conf alation_analytics-v2.pgsql.password -s 'password@123'"
+aa_postgres_password_master_cmd="chroot /opt/alation/alation /bin/sh -c 'su - alation -c \""$set_aa_postgres_password_cmd"\"'"
+aa_ocf_alation_conf_master_cmd="chroot /opt/alation/alation /bin/sh -c 'su - alationadmin -c \""$aa_ocf_alation_conf_cmd"\"'"
 
-
-command1="chroot /opt/alation/alation /bin/sh -c 'su - alationadmin -c \""$alation_config_commands1"\"'"
-
-command2="chroot /opt/alation/alation /bin/sh -c 'su - alation -c \""$alation_config_commands2"\"'"
-
-kubectl exec $pod -- bash -c "$command2"
-kubectl exec $pod -- bash -c "$command1"
+kubectl exec $pod -- bash -c "$aa_postgres_password_master_cmd"
+kubectl exec $pod -- bash -c "$aa_ocf_alation_conf_master_cmd"
