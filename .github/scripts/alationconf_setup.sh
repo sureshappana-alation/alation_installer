7523b9642649d57a39384fc06bb622b8638d58ff
Suresh Appana <suresh.appana@alation.com>
2021-10-01 11:31:53 -0400

    Workflow to automate the manual steps

--- .github/scripts/alationconf_setup.sh
+++ .github/scripts/alationconf_setup.sh
@@ -5,11 +5,11 @@ set -e
 
 aws eks --region us-east-2 update-kubeconfig --name CloudInfraQAEKS
 
-pod=$(kubectl get pod -l app.kubernetes.io/name=alationfc -o jsonpath="{.items[0].metadata.name}")
+alationfc_pod_name=$(kubectl get pod -l app.kubernetes.io/name=alationfc -o jsonpath="{.items[0].metadata.name}")
 
-echo "Waiting for Alation fat container pod "$pod " to become ready"
+echo "Waiting for Alation fat container pod "$alationfc_pod_name " to become ready"
 
-kubectl wait --for=condition=ready pod $pod
+kubectl wait --for=condition=ready pod $alationfc_pod_name
 
 
 aa_postgres_pwd_alation_conf_cmd="alation_conf alation_analytics-v2.pgsql.password -s 'password@123'"
@@ -24,5 +24,5 @@ alation_supervisor restart web:* celery:*"
 aa_postgres_password_master_cmd="chroot /opt/alation/alation /bin/sh -c 'su - alation -c \""$set_aa_postgres_password_cmd"\"'"
 aa_ocf_alation_conf_master_cmd="chroot /opt/alation/alation /bin/sh -c 'su - alationadmin -c \""$aa_ocf_alation_conf_cmd"\"'"
 
-kubectl exec $pod -- bash -c "$aa_postgres_password_master_cmd"
-kubectl exec $pod -- bash -c "$aa_ocf_alation_conf_master_cmd"
+kubectl exec $alationfc_pod_name -- bash -c "$aa_postgres_password_master_cmd"
+kubectl exec $alationfc_pod_name -- bash -c "$aa_ocf_alation_conf_master_cmd"
